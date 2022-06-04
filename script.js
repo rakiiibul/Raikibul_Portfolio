c9be5b8a41a87f4bd621c6b93dded0d55651460d
rakiiibul <58264212+rakiiibul@users.noreply.github.com>
2022-06-05 01:36:58 +0200

    update

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ $(document).ready(function(){
     });
     //typing animation screen
     var typed=new Typed(".typing-1",{
-        strings:["Programmer","Developer","Webdesigner"],
+        strings:["Programmer","Data scientist","Data analyst"],
         typeSpeed:100,
         backSpeed:60,
         loop:true
     });
     var typed=new Typed(".typing-2",{
-        strings:["Programmer","Developer","Webdesigner"],
+        strings:["Programmer","Data scientist","Data analyst"],
         typeSpeed:100,
         backSpeed:60,
         loop:true
